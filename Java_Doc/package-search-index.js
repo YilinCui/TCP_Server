540eb7c1d6192c391486e420cff94bc6add438a1
Yilin Cui <99826136+YilinCui@users.noreply.github.com>
2023-07-24 09:52:03 -0700

    Delete Java_Doc directory

--- Java_Doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Constant"},{"l":"Controller"},{"l":"DataStructure"},{"l":"ParseData"},{"l":"pgdata"}];updateSearchResults();
